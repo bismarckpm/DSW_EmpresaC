ecfa40282fc40f87b3b7d7bd7d7bbd0a5db250de
Didi <medinadiorfelis@gmail.com>
2020-11-26 04:44:34 -0400

    inicio

--- Angular-desarrollo/src/app/app-routing.module.ts
+++ Angular-desarrollo/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnalistaComponent } from './analista/analista.component';
+import {EncuestadoComponent} from './encuestado/encuestado.component';
+import {ClienteComponent} from './cliente/cliente.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:AnalistaComponent},
+  {path:'cliente-home', component:ClienteComponent},
+  {path:'encuestado-home', component:EncuestadoComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
