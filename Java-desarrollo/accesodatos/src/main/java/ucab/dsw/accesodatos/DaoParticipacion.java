3fd8cddbfbc80749bc252d2f47df0b35500302c3
Gabriel Romero <garomerol.est.ucab.edu.ve>
2020-12-11 23:59:39 -0430

    EstudiosByAnalista

--- Java-desarrollo/accesodatos/src/main/java/ucab/dsw/accesodatos/DaoParticipacion.java
+++ Java-desarrollo/accesodatos/src/main/java/ucab/dsw/accesodatos/DaoParticipacion.java
@@ -1,8 +1,12 @@
 package ucab.dsw.accesodatos;
 
 import ucab.dsw.entidades.Participacion;
+import ucab.dsw.entidades.SolicitudEstudio;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class DaoParticipacion extends Dao<Participacion>{
     private EntityManager _em;
     static DaoHandler _handler = new DaoHandler();
@@ -11,5 +15,15 @@ public class DaoParticipacion extends Dao<Participacion>{
     public DaoParticipacion( )
     {
         super( _handler );
+        this._em=_handler.getSession();
     }
+
+    public List<Participacion> getParticipacionByEstudio(long _id){
+        TypedQuery<Participacion> participacionByEstudio= this._em.createNamedQuery("ParticipacionByEstudio", Participacion.class);
+        participacionByEstudio.setParameter("estudio_id", _id);
+        List<Participacion> resultList= participacionByEstudio.getResultList();
+
+        return resultList;
+    }
+
 }
