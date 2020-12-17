df1b0d3fdcd97fd0818a946e4beca4c02ecc3e90
Gabriel Romero <garomerol.est.ucab.edu.ve>
2020-12-17 00:43:43 -0430

    Cambios Verificados. OK

--- Angular-desarrollo/src/app/comun/componentes/aside/modal-password/modal-password.component.ts
+++ Angular-desarrollo/src/app/comun/componentes/aside/modal-password/modal-password.component.ts
@@ -36,8 +36,9 @@ export class ModalPasswordComponent implements OnInit {
       (response)=>{
 		  console.log(response);
 		  if(response.estado=='success'){
-			    this._toastrService.success("Exito", "Contraseña cambiada");
-				this._toastrService.info('Espero un momento, por favor.','Actualizando...');
+			  this._toastrService.success("Exito", "Contraseña cambiada");
+        this._toastrService.info('Espero un momento, por favor.','Actualizando...');
+        this.eventBus.cast('fin-progress','chao');
 				this.eventBus.cast('cerrar-modal-password','cerrar');
 		  }
 		  else{
