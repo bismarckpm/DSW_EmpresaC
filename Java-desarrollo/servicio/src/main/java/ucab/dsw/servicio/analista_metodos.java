e5b28a6010cee77ac8b87e470dbb252d6054cb09
carlosdsr <50451182+carlosdsr@users.noreply.github.com>
2020-12-27 01:39:28 -0400

    cambios respuesta por estudio

--- Java-desarrollo/servicio/src/main/java/ucab/dsw/servicio/analista_metodos.java
+++ Java-desarrollo/servicio/src/main/java/ucab/dsw/servicio/analista_metodos.java
@@ -424,66 +424,78 @@ public class analista_metodos {
         JsonObject data;
         JsonArrayBuilder builder = Json.createArrayBuilder();
         JsonArrayBuilder respuesta_opcion =Json.createArrayBuilder();
+        JsonArrayBuilder respuesta_participacion =Json.createArrayBuilder();
         JsonObject builderObject;
         try {
             List<Respuesta> resultado = null;
             List<RespuestaOpcion> resultado2 = null;
+            List<Participacion> resultado3 = null;
 
             DaoRespuesta dao = new DaoRespuesta();
             DaoRespuestaOpcion dao2 = new DaoRespuestaOpcion();
             DaoSolicitudEstudio dao3 = new DaoSolicitudEstudio();
+            DaoParticipacion dao4 = new DaoParticipacion();
 
             SolicitudEstudio solicitudEstudio = new SolicitudEstudio();
             solicitudEstudio = dao3.find(_id,SolicitudEstudio.class);
 
             Class<Respuesta> type = Respuesta.class;
             Class<RespuestaOpcion> type2 = RespuestaOpcion.class;
+            Class<Participacion> type3 = Participacion.class;
 
             resultado = dao.findAll(type);
             resultado2 = dao2.findAll(type2);
+            resultado3 = dao4.findAll(type3);
 
-            for (Respuesta obj : resultado) {
-                Respuesta respuesta = dao.find(obj.get_id(), Respuesta.class);
-                if (respuesta.get_participacion().get_solicitudestudio().get_id()== solicitudEstudio.get_id()){
+            for (Participacion obj3 : resultado3) {
+                Participacion participacion = dao4.find(obj3.get_id(), Participacion.class);
+                if (participacion.get_solicitudestudio().get_id() == solicitudEstudio.get_id() && participacion.get_estado().equals("inactivo")) {
+                    for (Respuesta obj : resultado) {
+                        Respuesta respuesta = dao.find(obj.get_id(), Respuesta.class);
+                        if (respuesta.get_participacion().get_id() == participacion.get_id()) {
 
-                    if(respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("desarrollo") ) {
-                        JsonObject p = Json.createObjectBuilder()
-                                .add("encuestado", respuesta.get_participacion().get_encuestado().get_nombre())
-                                .add("pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_descripcion())
-                                .add("respuesta", respuesta.get_respuestadesarrollo()).build();
-                        builder.add(p);
-                    }
-                    if(respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("booleana") ) {
-                        JsonObject p = Json.createObjectBuilder()
-                                .add("encuestado", respuesta.get_participacion().get_encuestado().get_nombre())
-                                .add("pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_descripcion())
-                                .add("respuesta", respuesta.get_respuestaboolean()).build();
-                        builder.add(p);
-                    }
-                    if(respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("rango") ) {
-                        JsonObject p = Json.createObjectBuilder()
-                                .add("encuestado", respuesta.get_participacion().get_encuestado().get_nombre())
-                                .add("pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_descripcion())
-                                .add("respuesta", respuesta.get_respuestarango()).build();
-                        builder.add(p);
-                    }
+                            if (respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("desarrollo")) {
+                                respuesta_participacion.add(Json.createObjectBuilder()
+                                        .add("pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_descripcion())
+                                        .add("tipo pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta())
+                                        .add("respuesta", respuesta.get_respuestadesarrollo()));
+                            }
+                            if (respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("booleana")) {
+                                respuesta_participacion.add(Json.createObjectBuilder()
+                                        .add("pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_descripcion())
+                                        .add("tipo pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta())
+                                        .add("respuesta", respuesta.get_respuestaboolean()));
+                            }
+                            if (respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("rango")) {
+                                respuesta_participacion.add(Json.createObjectBuilder()
+                                        .add("pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_descripcion())
+                                        .add("tipo pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta())
+                                        .add("respuesta", respuesta.get_respuestarango()));
+                            }
 
-                    if(respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("Opcion simple") || respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("Opcion multiple")) {
-                        for (RespuestaOpcion obj2 : resultado2) {
-                            RespuestaOpcion respuesta_Opcion = dao2.find(obj2.get_id(), RespuestaOpcion.class);
-                            if (respuesta.get_id() == respuesta_Opcion.get_respuesta().get_id()){
-                                respuesta_opcion.add(Json.createObjectBuilder().add("respuestaOpcion", respuesta_Opcion.get_opcionsimplemultiple().get_opcionsimplemultiple().get_opcion()));
+                            if (respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("Opcion simple") || respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta().equals("Opcion multiple")) {
+                                for (RespuestaOpcion obj2 : resultado2) {
+                                    RespuestaOpcion respuesta_Opcion = dao2.find(obj2.get_id(), RespuestaOpcion.class);
+                                    if (respuesta.get_id() == respuesta_Opcion.get_respuesta().get_id()) {
+                                        respuesta_opcion.add(Json.createObjectBuilder().add("respuestaOpcion", respuesta_Opcion.get_opcionsimplemultiple().get_opcionsimplemultiple().get_opcion()));
+                                    }
+                                }
+                                respuesta_participacion.add(Json.createObjectBuilder()
+                                        .add("pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_descripcion())
+                                        .add("tipo pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_tipopregunta())
+                                        .add("respuesta", respuesta_opcion));
+                            }
                         }
-                        }
-                        JsonObject p = Json.createObjectBuilder()
-                                .add("encuestado", respuesta.get_participacion().get_encuestado().get_nombre())
-                                .add("pregunta", respuesta.get_preguntaencuesta().get_pregunta().get_descripcion())
-                                .add("respuesta", respuesta_opcion).build();
-                        builder.add(p);
+
+
                     }
+
+                    JsonObject p = Json.createObjectBuilder()
+                            .add("encuestado", participacion.get_encuestado().get_nombre())
+                            .add("respuestas", respuesta_participacion).build();
+                    builder.add(p);
+
                 }
-
-
             }
 
             data= Json.createObjectBuilder()
